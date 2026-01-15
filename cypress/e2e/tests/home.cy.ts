827a1cd467066423b727aa3cbb457eefed540eb4
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-15 11:12:23 -0700

    Added a test to validate page Images.

--- cypress/e2e/tests/home.cy.ts
+++ cypress/e2e/tests/home.cy.ts
@@ -10,7 +10,7 @@ describe('Test "Home" page by', () => {
     });
 
     it('validating its URL and title', () => {
-        cy.validateElementText({ selector: 'h1[id="jw-header-title"] > span', index: 0, expectedText: data.pageTitle });
+        cy.validateElementText({ selector: 'h1[id="jw-header-title"] > span', expectedText: data.pageTitle });
     })
 
     it('validating main headers contents', () => {
@@ -18,7 +18,7 @@ describe('Test "Home" page by', () => {
             cy.log(`Validating: ${item.headerText}`);
             cy.validateElementText({ selector: 'h1[class^="jw-heading"]', index: i, expectedText: item.headerText });
         });
-        cy.validateElementText({ selector: 'div[class="jw-slideshow-title"]', index: 0, expectedText: "Discover Unique Ways To Create Test Scripts" });
+        cy.validateElementText({ selector: 'div[class="jw-slideshow-title"]', expectedText: "Discover Unique Ways To Create Test Scripts" });
     })
 
     it('validating paragraph contents', () => {
@@ -26,34 +26,48 @@ describe('Test "Home" page by', () => {
             cy.log(`Validating: ${item.paragraphText}`);
             cy.validateElementText({ selector: 'div > p', index: i, expectedText: item.paragraphText });
         });
+        cy.validateElementText({ selector: 'h3', expectedText: `“Online Store offers an incredible variety of unique items that I can't find anywhere else!”` });
     });
 
-        it('validating top bar menu items and navigation to corresponding pages', () => {
-            const topBarMenuItems = [
-                { menuText: 'Home', href: '/' },
-                { menuText: 'Store', href: "/store" },
-                { menuText: 'FAQ', href: "/faq" },
-                { menuText: 'Customer Testimonials', href: "/customer-testimonials" },
-                { menuText: 'Contact', href: "/contact" },
-                { menuText: 'Elements with frames', href: "/elements-with-frames" },
-                { menuText: 'Showcase', href: "/showcase" },
-                { menuText: 'Clearance', href: "/clearance" }
-            ]
-            cy.wrap(topBarMenuItems).each((menuItem: any, i) => {
-                cy.log(`Validating menu item: ${menuItem.menuText}`);
-                cy.get(`a[class^="jw-menu-link"][href="${menuItem.href}"]`).should('exist').scrollIntoView({ timeout: 10000 }).within(() => {
-                    cy.get('span').should('contain.text', menuItem.menuText);
-                });
-                // Click on the menu item to navigate to the corresponding page.
-                cy.get(`a[class^="jw-menu-link"][href="${menuItem.href}"]`).click({ force: true });
-                cy.waitForPageToLoad();
-                cy.get(`a[class*="js-active-menu-item"][href="${menuItem.href}"]`).should('exist');
-                cy.url().should('include', menuItem.href, { timeout: 10000 });
+    it('validating top bar menu items and navigation to corresponding pages', () => {
+        const topBarMenuItems = [
+            { menuText: 'Home', href: '/' },
+            { menuText: 'Store', href: "/store" },
+            { menuText: 'FAQ', href: "/faq" },
+            { menuText: 'Customer Testimonials', href: "/customer-testimonials" },
+            { menuText: 'Contact', href: "/contact" },
+            { menuText: 'Elements with frames', href: "/elements-with-frames" },
+            { menuText: 'Showcase', href: "/showcase" },
+            { menuText: 'Clearance', href: "/clearance" }
+        ]
+        cy.wrap(topBarMenuItems).each((menuItem: any, i) => {
+            cy.log(`Validating menu item: ${menuItem.menuText}`);
+            cy.get(`a[class^="jw-menu-link"][href="${menuItem.href}"]`).should('exist').scrollIntoView({ timeout: 10000 }).within(() => {
+                cy.get('span').should('contain.text', menuItem.menuText);
             });
-            // Validate the 'About Us' button navigation.
-            cy.get('a[title="About Us"][href="/contact"]').should('exist').should('contain.text', 'About Us').scrollIntoView({ timeout: 10000 }).click({ force: true });
+            // Click on the menu item to navigate to the corresponding page.
+            cy.get(`a[class^="jw-menu-link"][href="${menuItem.href}"]`).click({ force: true });
             cy.waitForPageToLoad();
-            cy.url().should('include', '/contact', { timeout: 10000 });
+            cy.get(`a[class*="js-active-menu-item"][href="${menuItem.href}"]`).should('exist');
+            cy.url().should('include', menuItem.href, { timeout: 10000 });
+        });
+        // Validate the 'About Us' button navigation.
+        cy.get('a[title="About Us"][href="/contact"]').should('exist').should('contain.text', 'About Us').scrollIntoView({ timeout: 10000 }).click({ force: true });
+        cy.waitForPageToLoad();
+        cy.url().should('include', '/contact', { timeout: 10000 });
+    });
+
+    it('validating page images', () => {
+        let pictureCount: number;
+        cy.get('img').its('length').then((length: number) => {
+            pictureCount = length;
+            assert.equal(pictureCount, 5, 'Image count should be 5 on the Home page');
+            cy.log(`Total images on the page: ${pictureCount}`);
+            for (let i = 0; i < pictureCount; i++) {
+                cy.validateElementAttribute({ selector: 'picture[class^="jw-element-image__image-wrapper"] > img', attribute: 'style' });
+            }
         });
 
     });
+
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,7 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import type { Chainable } from "cypress";
 
 export { }; // Marks this file as a module
 
@@ -37,15 +36,34 @@ Cypress.Commands.add('navigateToUrl', (url: string) => {
 });
 
 /**
- * Validate that an element contains the expected text.
- * @example cy.validateElementText('h1.title', 0, 'Welcome')
+ * Validate that an element contains the expected text (eleement index is optional and defaults to 0).
  * @example cy.validateElementText('h1.title', 'Welcome')
+ * @example cy.validateElementText('h1.title', 0, 'Welcome')
  */
-Cypress.Commands.add('validateElementText', ({selector, index, expectedText}: {selector: string, index?: number, expectedText: string}) => {
+Cypress.Commands.add('validateElementText', ({ selector, index = 0, expectedText }) => {
     if (index !== undefined) {
         cy.get(selector).eq(index).should('exist').scrollIntoView({ timeout: 10000 }).should('have.text', expectedText);
     } else {
         cy.get(selector).should('exist').scrollIntoView({ timeout: 10000 }).should('have.text', expectedText);
+    }
+});
+
+/**
+ * Validate element attribute with OR without expected value.
+ * @example cy.validateElementAttribute('a.link', 'href')
+ * @example cy.validateElementAttribute('a.link', 0, 'href')
+ * @example cy.validateElementAttribute('a.link', 0, 'href', '/home')
+ */
+Cypress.Commands.add('validateElementAttribute', ({selector, index = 0, attribute, expectedValue}) => {
+    // Start the command chain
+    const element = cy.get(selector).eq(index).should('exist').scrollIntoView({ timeout: 10000 });
+
+    if (expectedValue !== undefined) {
+        // Assert that the attribute equals the specific value
+        element.should('have.attr', attribute, expectedValue);
+    } else {
+        // Only assert that the attribute exists
+        element.should('have.attr', attribute);
     }
 });
 
@@ -64,7 +82,13 @@ declare global {
     namespace Cypress {
         interface Chainable {
             navigateToUrl(url: string): Chainable<void>;
-            validateElementText({selector, index, expectedText}: {selector: string, index: number, expectedText: string}): Chainable<void>;
+            validateElementText(options: { selector: string, index?: number, expectedText: string }): Chainable<void>;
+            validateElementAttribute(options: {
+                selector: string;
+                index?: number; // Optional
+                attribute: string;
+                expectedValue?: string; // Optional
+            }): Chainable<void>;
             waitForPageToLoad(): Chainable<void>;
         }
     }
