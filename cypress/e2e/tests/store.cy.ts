d6bcc85f25555b8f4109f8181b72f608c07dbc6b
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-16 09:05:34 -0700

    Initial set-up.

---
+++ cypress/e2e/tests/store.cy.ts
@@ -0,0 +1,19 @@
+import * as data from '../../fixtures/store-data.json'
+import { StorePage } from '../POM/store-page';
+const storePage = new StorePage();
+
+describe('Testing the "Store" page by validating', () => {
+
+    beforeEach(() => {
+        cy.visit('/store');
+        cy.waitForPageToLoad();
+        cy.url().should('include', '/store', { timeout: 10000 });
+    });
+
+    it('the presence of all product items on the Store page', () => {
+        data.products.forEach((product) => {
+            
+        });
+    });
+
+});
