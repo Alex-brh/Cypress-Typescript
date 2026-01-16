7d89a8dc87ed96b49f856a4a64471464f97657b9
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-16 11:56:54 -0700

    Validate the the top disclaimer and the "Type here" button.

--- cypress/e2e/tests/store.cy.ts
+++ cypress/e2e/tests/store.cy.ts
@@ -10,6 +10,23 @@ describe('Testing the "Store" page by validating', () => {
         cy.url().should('include', '/store', { timeout: 10000 });
     });
 
+    it.only('the top disclaimer and the "Type here" button', () => {
+        // Validate the top disclaimer text.
+        cy.validateElementText({
+            selector: 'div[class="jw-element-imagetext-text"] > p > span',
+            expectedText: "It's being used for educational purposes ONLY. Nothing can be purchased and/or delivered through this website"
+        });
+        cy.validateElementText({
+            selector: 'div[class="jw-element-imagetext-text"] > p > span',
+            expectedText: "DISCLAIMER: This is NOT a real e-comm"
+        });
+        // Validate the "Type here" button text.
+        cy.validateElementText({
+            selector: 'a[class^="jw-element-content jw-btn"]',
+            expectedText: "Type here"
+        });
+    });
+
     it('the presence of all product items on the Store page', () => {
         // Validate the presence of all product items on the Store page.
         storePage.validateStorePageProducts({ data: data });
