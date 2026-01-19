9fac220d54625ba4346bc6e6d047d303cf737f59
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-19 14:40:14 -0700

    Added a test to validate 'each question and answer text'.

--- cypress/e2e/tests/faq.cy.ts
+++ cypress/e2e/tests/faq.cy.ts
@@ -13,8 +13,20 @@ describe('Test "FAQ" page by validating', () => {
         cy.url().should('include', '/faq', { timeout: 10000 });
     });
 
-    it('URL and title', () => {
+    it('URL and page top question and answer', () => {
+        // Validate the top question.
         cy.validateElementText({ selector: 'div[class="jw-element-imagetext-text"] > p', index: 0, expectedText: data.faqs[0].question });
+        // Validate the top answer.
+        cy.validateElementText({ selector: 'div[class="jw-element-imagetext-text"]', index: 0, expectedText: data.faqs[0].answer });
     })
+
+    it('each question and answer text', () => {
+        data.textInClosedSections.forEach((faq, index) => {
+            // Click a caret to expand the question/answer.
+            cy.clickSmart({ selector: 'i[class="jw-element-accordion__icon website-rendering-icon-right-open"]', index: index });
+            // Validate the answer text.
+            cy.validateElementText({ selector: 'div[class="jw-element-accordion__content-wrap"] > p > span', index: index, expectedText: faq.question });
+        });
+    });
 
 });
