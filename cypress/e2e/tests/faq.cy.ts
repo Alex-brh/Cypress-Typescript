ea4a7ebb3625491ec0b80d1f2bf2794c2f89d48d
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-20 11:31:05 -0700

    Added a method to the 'faq-page'.

--- cypress/e2e/tests/faq.cy.ts
+++ cypress/e2e/tests/faq.cy.ts
@@ -1,11 +1,9 @@
 // One way to import data from fixture files.
 import * as data from '../../fixtures/faq-data.json'
-import { HomePage } from '../POM/home-page';
-const homePage = new HomePage();
+import { FaqPage } from '../POM/faq-page';
+const faqPage = new FaqPage();
 
 describe('Test "FAQ" page by validating', () => {
-    // Another way to import data from fixture files.
-    const homeData = require('../../fixtures/home-data.json');
 
     beforeEach(() => {
         cy.navigateToUrl('/faq');
@@ -15,9 +13,9 @@ describe('Test "FAQ" page by validating', () => {
 
     it('URL and page top question and answer', () => {
         // Validate the top question.
-        cy.validateElementText({ selector: 'div[class="jw-element-imagetext-text"] > p', index: 0, expectedText: data.faqs[0].question });
+        faqPage.validateTopQuestionAndAnswer({ faqsData: { locator: 'div[class="jw-element-imagetext-text"] > p', index: 0, question: data.faqs[0].question } });
         // Validate the top answer.
-        cy.validateElementText({ selector: 'div[class="jw-element-imagetext-text"]', index: 0, expectedText: data.faqs[0].answer });
+        faqPage.validateTopQuestionAndAnswer({ faqsData: { locator: 'div[class="jw-element-imagetext-text"]', index: 0, answer: data.faqs[0].answer } });
     })
 
     it('each question and answer text', () => {
@@ -26,6 +24,26 @@ describe('Test "FAQ" page by validating', () => {
             cy.clickSmart({ selector: 'i[class="jw-element-accordion__icon website-rendering-icon-right-open"]', index: index });
             // Validate the answer text.
             cy.validateElementText({ selector: 'div[class="jw-element-accordion__content-wrap"] > p > span', index: index, expectedText: faq.question });
+        });
+    });
+
+    it('image visibility', () => {
+        cy.validateElementAttribute({
+            selector: 'img[class^="jw-element-image__image"]',
+            attribute: 'src'
+        });
+    });
+
+    it('Webador banner visibility', () => {
+        cy.validateElementText({
+            selector: 'div[class="jw-comment"] > h3',
+            index: 0,
+            expectedText: 'Create Your Own Website With'
+        });
+        cy.validateElementText({
+            selector: 'h3 > a > span',
+            index: 0,
+            expectedText: 'Webador'
         });
     });
 
