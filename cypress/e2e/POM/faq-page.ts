d44370a546b5e5dc69bb630055e742c4790c03a9
Alex Brohin <133638105+Alex-brh@users.noreply.github.com>
2026-01-20 12:58:16 -0700

    Add FAQ Page Test Suite with POM Pattern. (#3)

---
+++ cypress/e2e/POM/faq-page.ts
@@ -0,0 +1,27 @@
+
+// Define the interface for the options object.
+interface FaqData {
+    faqsData: { locator: string; index?: number; question?: string; answer?: string };
+}
+
+export class FaqPage {
+
+    validateTopQuestionAndAnswer(options: FaqData): void {
+        // Validate the top question and/or answer based on provided options.
+        if (options.faqsData.locator &&
+            options.faqsData.index !== undefined &&
+            (options.faqsData.question !== undefined || options.faqsData.answer !== undefined)) {
+            // Validate question if provided.
+            if (options.faqsData.question !== undefined) {
+                cy.validateElementText({ selector: options.faqsData.locator, index: options.faqsData.index, expectedText: options.faqsData.question });
+            }
+            // Validate answer if provided.
+            if (options.faqsData.answer !== undefined) {
+                cy.validateElementText({ selector: options.faqsData.locator, index: options.faqsData.index, expectedText: options.faqsData.answer });
+            }
+        } else {
+            throw new Error('Invalid parameters provided to validateTopQuestionAndAnswer method.');
+        }
+    }
+
+}
