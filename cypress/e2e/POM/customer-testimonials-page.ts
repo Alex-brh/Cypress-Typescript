7d145ef2f9437101f7522e6e2380277f45113e86
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-21 09:42:25 -0700

    Refactoring of the method.

--- cypress/e2e/POM/customer-testimonials-page.ts
+++ cypress/e2e/POM/customer-testimonials-page.ts
@@ -3,7 +3,7 @@
 export class CustomerTestimonialsPage {
 
     validateErrorMessages(options: { expectedText: Array<string> }): void {
-      // Click the 'Submit comment' button when the form is not populated
+        // Click the 'Submit comment' button when the form is not populated
         cy.clickSmart({ selector: 'button[name="submit"]', index: 0 });
         // Wait for the error messages to appear.
         cy.waitForPageToLoad();
@@ -18,15 +18,18 @@ export class CustomerTestimonialsPage {
         // Validate the error messages.
         const expectedText = options.expectedText
         cy.get('div[class="jw-element-form-error jw-comment-error"] > ul > li')
+            .should('have.length', expectedText.length) // Ensure the count matches
+            .each(($el, index) => {
+                // Log the individual text.
+                const text = $el.text().trim();
+                cy.log(`Element at index ${index} has text: ${text}`);
+
+                // Individual validation for each element.
+                cy.wrap($el).should('have.text', expectedText[index]);
+            })
             .then(($els) => {
-                // Get the text content of each element.
+                // Array validation.
                 const actualText = Cypress._.map($els, 'innerText');
-                // Log the text content.
-                cy.wrap(actualText).then((arr) => {
-                    cy.log(JSON.stringify(arr));
-                });
-                // Validate the text content.
-                expect(actualText).to.have.members(expectedText);
                 expect(actualText).to.deep.equal(expectedText);
             });
     }
