3e4f8f330567009d81b7089fcc6bf2fe365557c3
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-22 12:51:09 -0700

    Added each image attribute validation.

--- cypress/e2e/tests/showcase.cy.ts
+++ cypress/e2e/tests/showcase.cy.ts
@@ -6,6 +6,7 @@ describe('Test "Showcase" page by validating', () => {
 
     beforeEach(() => {
         cy.launchTheStore();
+        // Clik the "Showcase" menu item.
         cy.clickSmart({ selector: 'a[href="/showcase"]', index: 0 });
         cy.url().should('include', '/showcase', { timeout: 10000 });
         cy.waitForPageToLoad();
@@ -17,8 +18,16 @@ describe('Test "Showcase" page by validating', () => {
     })
 
     it('each paragraph text', () => {
-        cy.wrap(data.paragraphs).each((paragraph:  { paragraphText: string }, index) => { 
+        // Validate each paragraph text.
+        cy.wrap(data.paragraphs).each((paragraph: { paragraphText: string }, index) => {
             cy.validateElementText({ selector: 'p', index: index, expectedText: paragraph.paragraphText });
-        }); 
+        });
+    });
+
+    it('each image on the page', () => {
+        // Validate each image attribute.
+        cy.wrap(data.images).each((image: { attribute: string }, index) => {
+            cy.validateElementAttribute({ selector: 'img[class^="jw-element-image__image"]', index: index, attribute: image.attribute });
+        });
     });
 });
