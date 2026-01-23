e4a59c02cbee2284baa0535e2f895713640e4556
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-23 13:35:15 -0700

    Created validateLinksWithShadowDom().

--- cypress/e2e/POM/frames-page.ts
+++ cypress/e2e/POM/frames-page.ts
@@ -12,4 +12,32 @@ export class FramesPage {
         cy.url().should('include', '/elements-with-frames', { timeout: 10000 });
         cy.waitForPageToLoad();
     }
+
+    validateLinksWithShadowDom(frameData: { brand: string; href: string }[]): void {
+        cy.wrap(frameData).each((frame: { brand: string; href: string }) => {
+            // First count the number of iframe elements on the page.
+            cy.get('div[class="iframely-embed"] > div').should('have.length', 6);
+            // Get the link button inside the shadow DOM.
+            cy.get('div[class="iframely-embed"] > div', { timeout: 7000 })
+                .eq(frame.brand === "amazon" ? 0 : 5)
+                .shadow()
+                .find(`a[href="${frame.href}"]`)
+                .first().as("linkButton");
+
+            // Validate link button existence and visibility.   
+            cy.get("@linkButton")
+                .should("exist")
+                .scrollIntoView({ timeout: 10000 })
+                .should("be.visible");
+
+            // Validate link button navigation.
+            cy.get("@linkButton")
+                .invoke('removeAttr', 'target') // Prevents opening in a new tab.
+                .click({ force: true });
+            // NOTE: Removed page URL validation, as Amazon returns an error intermittently.
+            // Navigate back to blank page.
+            cy.visit('about:blank');
+            this.navigateToElementsWithFramesPage();
+        });
+    }
 }
