aa395e6e1b6c420c28b8ae228e3abe103d4af201
Alex-TDS\Alex <alex.brohin@everflowutilities.com>
2026-01-28 13:02:02 -0700

    Added failed tests retries.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    retries: {
+      // Re-run failed tests in 'cypress run' (CI/CD) a specified number of times.
+      runMode: 1,
+      // No retries for 'cypress open' (Local development).
+      openMode: 0,
+    },
      watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
